ab52161cda860e63909ab2a65e593f6edb5e200c
Nathan Woodruff <nwoodr94@gmail.com>
2021-02-09 13:43:57 -0700

    setup testing, separate app and server for testing

--- pipeline/server/server.js
+++ pipeline/server/server.js
@@ -1,25 +1,5 @@
 // Copyright 2020, Battelle Energy Alliance, LLC  ALL RIGHTS RESERVED
 
-// Express
-const express = require('express');
-const app = express();
-
-// Configuration
-require('dotenv').config();
-const host = process.env.SERVER_HOST;
-const port = process.env.SERVER_PORT;
-
-// Middleware
-const cors = require('cors');
-const corsOptions = { origin: '*'}; // Dev environment only
-app.use(cors(corsOptions));
-
-app.get('/test', function(req, res) {
-    res.send('test');
-})
-
-// Innoslate Router
-const innoslateRouter = require('./routers/InnoslateRouter');
-app.use('/innoslate', innoslateRouter);
+const app = require('./app');
 
 app.listen(port, host, () => console.log(`Adapter server is listening on ${host}:${port}`));
