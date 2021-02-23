a6bcb031a0cc57dba5c904e2cf8fd9c00bc4b30d
Brennan K. Harris <89001003969017@fedidcard.gov>
2021-02-22 17:25:14 -0700

    added DeepLynx routes. removed Promise.all wrapper from axios.gets

--- adapters/adapter-template/server/routers/DeepLynxRouter.js
+++ adapters/adapter-template/server/routers/DeepLynxRouter.js
@@ -8,23 +8,21 @@ const deepLynxRouter = express.Router();
 const DeepLynxTransformer = require('../transformers/DeepLynxTransformer.js');
 
 // Configuration
-require('dotenv').config()
+require('dotenv').config();
 const host = process.env.DEEP_LYNX_ADDRESS;
-const deepLynxAPIkey = process.env.DEEP_LYNX_API_KEY
-const deepLynxAPIsecret = process.env.DEEP_LYNX_API_SECRET
-const deepLynxTokenExpiry = process.env.DEEP_LYNX_TOKEN_EXPIRY
+const deepLynxAPIkey = process.env.DEEP_LYNX_API_KEY;
+const deepLynxAPIsecret = process.env.DEEP_LYNX_API_SECRET;
+const deepLynxTokenExpiry = process.env.DEEP_LYNX_TOKEN_EXPIRY;
 
 // Routes
-deepLynxRouter.get('/health', async function(req, res, next) {
-
+deepLynxRouter.get('/health', async function(req, res) {
     let transformer = new DeepLynxTransformer(host);
     let data = await transformer.getHealth();
 
     res.send(data);
-})
+});
 
-deepLynxRouter.get('/get_token', async function(req, res, next) {
-
+deepLynxRouter.get('/get_token', async function(req, res) {
     let transformer = new DeepLynxTransformer(host);
     let data = await transformer.getToken(
       deepLynxAPIkey,
@@ -33,6 +31,20 @@ deepLynxRouter.get('/get_token', async function(req, res, next) {
     );
 
     res.send(data);
+});
+
+deepLynxRouter.get('/get_containers/:token', async function (req, res) {
+  let transformer = new DeepLynxTransformer(host, req.params["token"])
+  let data = await transformer.getContainers();
+
+  res.send(data);
+})
+
+deepLynxRouter.get('/get_datasources/:container_id/:token', async function (req, res) {
+  let transformer = new DeepLynxTransformer(host, req.params["token"])
+  let data = await transformer.getDatasources(req.params["container_id"]);
+
+  res.send(data);
 })
 
 module.exports = deepLynxRouter;
