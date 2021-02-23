a8c616622a96ba0de710f81c8afa10c2ee3ed69b
Brennan K. Harris <89001003969017@fedidcard.gov>
2021-02-22 17:25:14 -0700

    added DeepLynx routes. removed Promise.all wrapper from axios.gets

--- adapters/adapter-template/server/transformers/DatasourceTransformer.js
+++ adapters/adapter-template/server/transformers/DatasourceTransformer.js
@@ -12,160 +12,97 @@ class DatasourceTransformer {
     constructor(host, key) {
         this.host = host,
         this.key = key
-        this.data = {
-            users: [],
-            posts: []
-        }
+        this.data = {}
     }
 
     async getAllUsers() {
-        /*
-            If there is only one user, the response.data object is not iterable and must be handled more verbosely.
-        */
-        await Promise.all([axios.get(`${this.host}/users`
+        await axios.get(`${this.host}/users`
           //,{headers: {'Authorization': `basic ${this.key}`}}  // UNCOMMENT if source requires auth
-        )
-        ]).then(responses => {
-            responses.forEach(response => {
+        ).then(response => {
+            this.data.users = [];
+            response.data.forEach(user => {
                 try {
-                    response.data.forEach(user => {
-                        this.data.users.push(
-                            new User(
-                              user.id,
-                              user.name,
-                              user.username,
-                              user.email,
-                              new Address (
+                    this.data.users.push(
+                        new User(
+                            user.id,
+                            user.name,
+                            user.username,
+                            user.email,
+                            new Address (
                                 user.address.street,
                                 user.address.suite,
                                 user.address.city,
                                 user.address.zipcode,
                                 new Geolocation(
-                                  user.address.geo.lat,
-                                  user.address.geo.lng
+                                    user.address.geo.lat,
+                                    user.address.geo.lng
                                 )
-                              ),
-                              user.phone,
-                              user.website,
-                              new Company(
+                            ),
+                            user.phone,
+                            user.website,
+                            new Company(
                                 user.company.name,
                                 user.company.catchPhrase,
                                 user.company.bs
-                              )
                             )
-                        )
-                    });
-                }
-                catch (error) {
-                    if (error instanceof TypeError) {
-                        this.data.projects.push(
-                            new User(
-                              response.data.id,
-                              response.data.name,
-                              response.data.username,
-                              response.data.email,
-                              new Address (
-                                response.data.address.street,
-                                response.data.address.suite,
-                                response.data.address.city,
-                                response.data.address.zipcode,
-                                new Geolocation(
-                                  response.data.address.geo.lat,
-                                  response.data.address.geo.lng
-                                )
-                              ),
-                              response.data.phone,
-                              response.data.website,
-                              new Company(
-                                response.data.company.name,
-                                response.data.company.catchPhrase,
-                                response.data.company.bs
-                              )
-                            )
-                        );
-                    }
-                    else {
-                        console.log(error);
-                    }
-                }
-            });
-        });
-
-        return this.data;
-
-    }
-
-    async getAllPosts() {
-        /*
-            If there is only one user, the response.data object is not iterable and must be handled more verbosely.
-        */
-        await Promise.all([axios.get(`${this.host}/posts`
-          //,{headers: {'Authorization': `basic ${this.key}`}}  // UNCOMMENT if source requires auth
-        )
-        ]).then(responses => {
-            responses.forEach(response => {
-                try {
-                    response.data.forEach(post => {
-                        this.data.posts.push(
-                            new Post(
-                              post.userId,
-                              post.id,
-                              post.title,
-                              post.body
-                            )
-                        )
-                    });
-                }
-                catch (error) {
-                    if (error instanceof TypeError) {
-                        this.data.posts.push(
-                            new Post(
-                              response.data.userId,
-                              response.data.id,
-                              response.data.title,
-                              response.data.body
-                            )
-                        );
-                    }
-                    else {
-                        console.log(error);
-                    }
-                }
-            });
-        });
-
-        return this.data;
-
-    }
-
-    async getSinglePost(ident) {
-        /*
-            If there is only one user, the response.data object is not iterable and must be handled more verbosely.
-        */
-        await Promise.all([axios.get(`${this.host}/posts/${ident}`
-          //,{headers: {'Authorization': `basic ${this.key}`}}  // UNCOMMENT if source requires auth
-        )
-        ]).then(responses => {
-            responses.forEach(response => {
-                try {
-                    this.data.posts.push(
-                        new Post(
-                          response.data.userId,
-                          response.data.id,
-                          response.data.title,
-                          response.data.body
                         )
                     )
-
                 }
                 catch (error) {
                     console.log(error);
+                    this.data.error = error;
                 }
             });
         });
-
         return this.data;
+    }
 
+    async getAllPosts() {
+        await axios.get(`${this.host}/posts`
+          //,{headers: {'Authorization': `basic ${this.key}`}}  // UNCOMMENT if source requires auth
+        ).then(response => {
+            this.data.posts = [];
+            response.data.forEach(post => {
+                try {
+                    this.data.posts.push(
+                        new Post(
+                            post.userId,
+                            post.id,
+                            post.title,
+                            post.body
+                        )
+                    )
+                }
+                catch (error) {
+                    console.log(error);
+                    this.data.error = error;
+                }
+            });
+        });
+        return this.data;
+    }
+
+    async getSinglePost(ident) {
+        await axios.get(`${this.host}/posts/${ident}`
+          //,{headers: {'Authorization': `basic ${this.key}`}}  // UNCOMMENT if source requires auth
+        ).then(response => {
+            this.data.posts = [];
+            try {
+                this.data.posts.push(
+                    new Post(
+                      response.data.userId,
+                      response.data.id,
+                      response.data.title,
+                      response.data.body
+                    )
+                );
+            }
+            catch (error) {
+                console.log(error);
+                this.data.error = error;
+            }
+        });
+        return this.data;
     }
 
 }
--- adapters/adapter-template/server/transformers/DeepLynxTransformer.js
+++ adapters/adapter-template/server/transformers/DeepLynxTransformer.js
@@ -10,21 +10,23 @@ class DeepLynxTransformer {
 
     constructor(host, token="NONE") {
         this.host = host,
-        this.token = token
+        this.config = {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }
         this.data = {}
     }
 
     async getHealth() {
-        await axios.get(`${this.host}/health`
-        ).then(response => {
+        await axios.get(`${this.host}/health`).then(response => {
             try {
-              this.data = response.data
+                this.data = response.data;
             }
             catch (error) {
                 console.log(error);
             }
         });
-
         return this.data;
     }
 
@@ -32,22 +34,62 @@ class DeepLynxTransformer {
         await axios.get(`${this.host}/oauth/token`,
             {headers:
                 {
-                  'x-api-key': apiKey,
-                  'x-api-secret': apiSecret,
-                  'x-api-expiry': tokenExpiry
+                    'x-api-key': apiKey,
+                    'x-api-secret': apiSecret,
+                    'x-api-expiry': tokenExpiry
                 }
             }
         ).then(response => {
             try {
-              this.data.token = response.data
+                this.data.token = response.data;
             }
             catch (error) {
+                this.data.error = error;
                 console.log(error);
             }
         });
-
         return this.data;
+    }
 
+    async getContainers() {
+        await axios.get(
+          `${this.host}/containers?offset=0&limit=100`,
+          this.config
+        ).then(response => {
+            this.data.containers = [];
+            response.data.value.forEach(container => {
+                this.data.containers.push({
+                    'name': container.name,
+                    'description': container.description,
+                    'id': container.id
+                })
+            })
+            //this.data.containers = response.data.value;
+        }).catch(error => {
+            this.data.error = error;
+        });
+        return this.data;
+    }
+
+    async getDatasources(container_id) {
+        await axios.get(
+          `${this.host}/containers/${container_id}/import/datasources`,
+          this.config
+        ).then(response => {
+            this.data.datasources = [];
+            response.data.value.forEach(datasource => {
+                this.data.datasources.push({
+                    'name': datasource.name,
+                    'id': datasource.id,
+                    'type': datasource.type,
+                    'config': datasource.config
+                })
+            })
+            //this.data.containers = response.data.value;
+        }).catch(error => {
+            this.data.error = error;
+        });
+        return this.data;
     }
 
 }
