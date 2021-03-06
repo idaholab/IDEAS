9f59696550aa7f6f141e5ebbd6ea8c7c7c9655f6
Brennan K. Harris <89001003969017@fedidcard.gov>
2021-03-05 17:34:35 -0700

    Added auth pathway to source selector. Added Vault item and file lists

--- adapters/deep-lynx-adapter/server/transformers/Vault/VaultTransformer.js
+++ adapters/deep-lynx-adapter/server/transformers/Vault/VaultTransformer.js
@@ -8,25 +8,110 @@ class VaultTransformer {
         this.host = host,
         this.token = token,
         this.user_id = user_id,
-        this.data = {
-          'objects': []
-        }
+        this.data = {}
     }
 
     async makeAuthURL(username, password, vault) {
-      let response = await axios.get(
-        `${this.host}/soap/filestore/IdentificationService/GetServerIdentities/NONE/NONE`
-      );
-      let dataserver = response.data.RESULT.GetServerIdentitiesResult.attributes.DataServer;
-      let url = `${this.host}/soap/filestore/AuthService/SignIn/NONE/NONE?dataServer=${dataserver}&userName=${username}&userPassword=${password}&knowledgeVault=${vault}`;
-      return url;
+        let response = await axios.get(
+          `${this.host}/soap/filestore/IdentificationService/GetServerIdentities/NONE/NONE`
+        );
+        let dataserver = response.data.RESULT.GetServerIdentitiesResult.attributes.DataServer;
+        let url = `${this.host}/soap/filestore/AuthService/SignIn/NONE/NONE?dataServer=${dataserver}&userName=${username}&userPassword=${password}&knowledgeVault=${vault}`;
+        return url;
     }
 
     async getTokenAndUser(auth_url) {
-      let response = await axios.get(auth_url);
-      let token = response.data.TICKET;
-      let user_id = response.data.USER_ID;
-      return {token, user_id};
+        let response = await axios.get(auth_url);
+        let token = response.data.TICKET;
+        let user_id = response.data.USER_ID;
+        return {token, user_id};
+    }
+
+    async getItems(token, user_id) {
+        let response = await axios.post(
+            `${this.host}/soap/standard/ItemService/FindItemRevisionsBySearchConditions/${token}/${user_id}`,
+            {
+                "searchConditions": {
+                    "PropDefId": "0",
+                    "SrchOper": "1",
+                    "SrchTxt": "",
+                    "PropTyp": "AllProperties",
+                    "SrchRule": "Must"
+                },
+                "sortConditions": {},
+                "bRequestLatestOnly": "false",
+                "bookmark": "xs:string"
+            }
+        );
+
+        let items = response.data.RESULT.FindItemRevisionsBySearchConditionsResult.Item
+        this.data.objects = [];
+        this.data.ids = [];
+        items.forEach(item => {
+            this.data.objects.push(
+                {
+                    "id": item.attributes.MasterId,
+                    "name": item.attributes.Title,
+                    "description": item.attributes.Detail
+                }
+            );
+            this.data.ids.push(item.attributes.Id);
+        });
+
+        return this.data;
+    }
+
+    async getSingleItemHistory(token, user_id, item_id) {
+        let response = await axios.get(
+            `${this.host}/soap/standard/ItemService/GetItemHistoryByItemMasterId/${token}/${user_id}?itemMasterId=${item_id}&historyTyp=All`
+        );
+        this.data.vault_item_revisions = response.data.RESULT.GetItemHistoryByItemMasterIdResult.Item;
+
+        return this.data;
+    }
+
+    async getSingleItemLatest(token, user_id, item_id) {
+        let response = await axios.get(
+            `${this.host}/soap/standard/ItemService/GetLatestItemByItemMasterId/${token}/${user_id}?itemMasterId=${item_id}`
+        );
+        this.data.vault_item_latest = response.data.RESULT.GetLatestItemByItemMasterIdResult.attributes;
+
+        return this.data;
+    }
+
+    async getFiles(token, user_id, items) {
+        let response = await axios.post(
+            `${this.host}/soap/standard/ItemService/GetAttachmentsByItemIds/${token}/${user_id}`,
+            {"itemIds": {"long": items}}
+        );
+        let item_entries = response.data.RESULT.GetAttachmentsByItemIdsResult.ItemAttmt;
+        let file_ids = [];
+        item_entries.forEach(item_entry => {
+            item_entry.AttmtArray.forEach(file => {
+                file_ids.push(parseInt(file.attributes.FileId))
+            });
+        });
+
+        file_ids = [...new Set(file_ids)];
+        console.log(file_ids);
+
+        let file_response = await axios.post(
+            `${this.host}/soap/standard/DocumentService/GetFilesByIds/${token}/${user_id}`,
+            {"fileIds": {"long": file_ids}}
+        );
+        let file_entries = file_response.data.RESULT.GetFilesByIdsResult.File;
+        this.data.objects = [];
+        file_entries.forEach(file_entry => {
+            this.data.objects.push(
+                {
+                    "id": file_entry.attributes.Id,
+                    "name": file_entry.attributes.Name,
+                    "description": "NONE"
+                }
+            );
+        });
+
+        return this.data;
     }
 
 
--- adapters/deep-lynx-adapter/ui/src/components/Selector.vue
+++ adapters/deep-lynx-adapter/ui/src/components/Selector.vue
@@ -40,31 +40,77 @@ const axios = require('axios');
       DeepLynx
     },
     methods: {
-      getObjects(urls) {
-        axios.get(urls[0]).then(response => {
-          this.objects = response.data.projects;
+      async getObjects(urls) {
+        let list_url = urls["list"]
+        let transform_url = urls["transform"]
+
+        if (urls["auth"]) {
+          let response = await axios.get(urls["auth"]);
+          this.token = response.data.token;
+          this.user_id = response.data.user_id;
+          list_url += `/${this.token}/${this.user_id}`;
+          transform_url += `/${this.token}/${this.user_id}`;
+        }
+
+        axios.get(list_url).then(response => {
+          this.objects = response.data.objects;
         }).catch(error => {
           this.error_message = error;
         });
-        this.transformURL = urls[1];
+
+        this.transformURL = transform_url;
       },
       setDeepLynxURL(url) {
         this.deepLynxURL = url;
       }
     },
-    data: () => ({
-      error_message: null,
-      objects: null,
-      sources: [
-        {
-          name: "Innoslate",
-          urls: [`http://${process.env.VUE_APP_UI_HOST}:${process.env.VUE_APP_SERVER_PORT}/innoslate`, // list source GET
-          `http://${process.env.VUE_APP_UI_HOST}:${process.env.VUE_APP_SERVER_PORT}/innoslate/document-data`] // transform GET
-        },
-      ],
-      deepLynxURL: null,
-      transformURL: null
-    })
+    data () {
+      let host = `http://${process.env.VUE_APP_UI_HOST}:${process.env.VUE_APP_SERVER_PORT}`;
+      return {
+        error_message: null,
+        objects: null,
+        token: null,
+        user_id: null,
+        sources: [
+          {
+            name: "Innoslate",
+            urls: {
+              "auth": null,
+              "list": `${host}/innoslate`, // list source GET
+              "transform": `${host}/innoslate/document-data` // transform GET
+            }
+          },
+          {
+            name: "Vault Items (History)",
+            urls: {
+              "auth": `${host}/vault/authenticate`,
+              "list": `${host}/vault/get_item_list`, // list source GET
+              "transform": `${host}/vault/get_single_item_history` // transform GET
+            }
+          },
+          {
+            name: "Vault Items (Latest)",
+            urls: {
+              "auth": `${host}/vault/authenticate`,
+              "list": `${host}/vault/get_item_list`, // list source GET
+              "transform": `${host}/vault/get_single_item_latest` // transform GET
+            }
+          },
+          {
+            name: "Vault Files",
+            urls: {
+              "auth": `${host}/vault/authenticate`,
+              "list": `${host}/vault/get_file_list`, // list source GET
+              "transform": `${host}/vault/get_single_item_latest` // transform GET
+            }
+          }
+
+        ],
+        deepLynxURL: null,
+        transformURL: null
+      }
+
+    }
   }
 </script>
 
