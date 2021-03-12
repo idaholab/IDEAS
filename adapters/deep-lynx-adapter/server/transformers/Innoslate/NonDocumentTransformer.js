9b3f0a730d41e28a4a4362db84bd8fb11fdabb70
Nathan Woodruff <nwoodr94@gmail.com>
2021-03-11 21:14:32 -0700

    entity extraction mapped to schemas

--- adapters/deep-lynx-adapter/server/transformers/Innoslate/NonDocumentTransformer.js
+++ adapters/deep-lynx-adapter/server/transformers/Innoslate/NonDocumentTransformer.js
@@ -3,6 +3,7 @@
 const Project = require('./models/Project');
 const Entity = require('./models/Entity');
 const Organization = require('./models/Organization');
+const Schema = require('./models/Schema');
 const axios = require('axios');
 
 class NonDocumentTransformer {
@@ -10,118 +11,38 @@ class NonDocumentTransformer {
     constructor(host, key) {
         this.host = host,
         this.key = key,
-        this.data = {
-            projects: [],
-            // We can add more properties here!
-        }
+        this.schema = new Map(),
+        this.data = {}
     }
 
     async extract(projId) {
-        await this.getProject(projId);
+        await this.extractSchema(projId);
         await this.extractEntities(projId);
-        await this.getOrganizations();
-
         return this.data;
     }
 
-    async getProject(projId) {
-    /*
-        Extract projects from the Innoslate API. 
-
-        If there is only one project, the response.data object is not iterable and must be handled more verbosely.
-    */
-        await Promise.all([axios.get(`${this.host}/o/nric/p/${projId}`, 
-        {
+    async extractSchema(projId) {
+        await axios.get(`${this.host}/o/nric/p/${projId}/schema`, {
             headers: {'Authorization': `basic ${this.key}`}
-        })
-        ]).then(responses => {
-            responses.forEach(response => {
-                try {
-                    response.data.forEach(project => {
-                        this.data.projects.push(
-                            new Project(
-                                project.id,
-                                project.name,
-                                project.description
-                            )
-                        )
-                    });
-                } 
-                catch (error) {
-                    if (error instanceof TypeError) {
-                        let id = response.data.id;
-                        let name = response.data.name;
-                        let description = response.data.description;
-                        this.data.projects.push(new Project(
-                            id,
-                            name,
-                            description
-                        ));
-                    }
-                    else {
-                        console.log(error);
-                    }
-                }
+        }).then(response => {
+            response.data.classes.forEach(cls => {
+                this.schema.set( cls.id, new Schema (
+                    cls.id,
+                    cls.name
+                    )
+                );
             });
         });
-    }
-
-    async getOrganizations() {
-        await Promise.all([axios.get(`${this.host}/o`, {
-            headers: {
-                'Authorization': `basic ${this.key}`
-            }
-        })]).then(responses => {
-            responses.forEach((response, project) => {
-                try {
-                    response.data.forEach(organization => {
-                        this.data.projects[project].organizations.push(
-                            new Organization(
-                                organization.slug,
-                                organization.name,
-                                organization.description,
-                                organization.created,
-                                organization.modified,
-                                organization.createdBy,
-                                organization.modifiedBy,
-                                organization.version
-                            )
-                        )
-                    })
-                }
-                catch (error) {
-                    if (error instanceof TypeError) {
-                        this.data.projects[project].organizations.push(
-                            new Organization(
-                                response.data.slug,
-                                response.data.name,
-                                response.data.description,
-                                response.data.created,
-                                response.data.modified,
-                                response.data.createdBy,
-                                response.data.modifiedBy,
-                                response.data.version
-                            )
-                        )
-                    }
-                    else {
-                        console.log(error)
-                    }
-                }
-            });
-        });
-
-        return this.data;
     }
 
     async extractEntities(projId) {
-        await Promise.all([axios.get(`${this.host}/o/nric/entities`, {
+        await axios.get(`${this.host}/o/nric/entities`, {
             params: {'query': 'order:modified-', 'projectId': projId}, 
             headers: {'Authorization': `basic ${this.key}`}
-        })]).then(responses => {
-            responses.forEach((response, project) => {
-                response.data.forEach(entity => {
-                    this.data.projects[project].entities.push(
+        }).then(response => {
+            response.data.forEach(entity => {
+                    let schema = this.schema.get(entity.classId);
+                    schema.entities.push(
                         new Entity (
                             entity.id,
                             entity.number,
@@ -138,10 +59,10 @@ class NonDocumentTransformer {
                             entity.rels,
                             entity.version
                         )
-                    );
+                    )
+                    this.data[`${schema.name}`] = schema.entities;
                 });
             });
-        });
     }
 }
 
--- adapters/deep-lynx-adapter/server/transformers/Innoslate/models/Schema.js
+++ adapters/deep-lynx-adapter/server/transformers/Innoslate/models/Schema.js
@@ -1,5 +1,11 @@
 // Copyright 2020, Battelle Energy Alliance, LLC  ALL RIGHTS RESERVED
 
 class Schema {
-    constructor() {}
+    constructor(id, name) {
+        this.id = id,
+        this.name = name,
+        this.entities = []
+    }
 }
+
+module.exports = Schema;
