5cf9f7f5bc357fa5d74a9d94ce3723cc8048f221
Brennan K. Harris <89001003969017@fedidcard.gov>
2021-04-05 17:08:01 -0600

    refreshed UI

--- reports-generator/client/src/App.vue
+++ reports-generator/client/src/App.vue
@@ -1,6 +1,6 @@
 Copyright 2020, Battelle Energy Alliance, LLC  ALL RIGHTS RESERVED
 
-<template>
+<!-- <template>
     <UI />
 </template>
 
@@ -17,6 +17,77 @@ export default {
         return {
             nameString: this.nameString
         };
+    }
+}
+</script> -->
+
+<template>
+    <v-app>
+        <v-main>
+            <v-container>
+                <h2>Innoslate Reports</h2>
+
+                <v-spacer>
+                    <br />
+                </v-spacer>
+
+                <input type="text" id="nameString" v-model="nameString" placeholder="Author(s)">
+                <v-btn @click="addName"><span class="material-icons">
+person_add_alt
+</span></v-btn>
+                <ul>
+                    <li v-for="name in nameArray">{{ name }}</li>
+                </ul>
+
+                <v-spacer>
+                    <br />
+                </v-spacer>
+
+                <v-btn large v-if="!this.projects.length" v-on:click="getProjects" text small color="#07519E">Get NRIC Projects</v-btn>
+
+                <template v-if="this.projects.length">
+                    <Projects v-bind:projects="projects" v-bind:nameString="nameConcat"/>
+                </template>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+import Projects from './components/Projects'
+import {getProjects, getTemplates} from './utils/utils';
+
+export default {
+    name: "App",
+    components: {
+        Projects
+    },
+    methods: {
+        getProjects: async function() {
+            this.projects = await getProjects();
+        },
+        addName: function() {
+            if (!this.nameString) {
+                return;
+            }
+            //this.nameList += this.nameString + "|";
+            this.nameArray.push(this.nameString)
+            this.nameConcat = this.nameArray.join("|")
+            this.nameString = '';
+            //alert(this.nameList);
+        }
+    },
+    data: function() {
+        return {
+            projects: [],
+            templates: [],
+            nameString: '',
+            nameConcat: '',
+            nameArray: []
+        }
+    },
+    mounted: function() {
+        this.templates = getTemplates();
     }
 }
 </script>
