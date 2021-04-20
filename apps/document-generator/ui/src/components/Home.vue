5933c7c6b74215ebdbec64e044cac502fc270075
Nathan Woodruff <nwoodr94@gmail.com>
2021-04-19 18:18:53 -0600

    document generator prototype - displays nodes

---
+++ apps/document-generator/ui/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    Welcome to the Document Generator
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    props: {
+      //
+    },
+    data: () => ({
+      //
+    }),
+  }
+</script>
